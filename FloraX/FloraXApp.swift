6277d799573c42d085c5efa1f1e30d96532fc27d
İlknur Tulgar <ilknur001-@hotmail.com>
2025-11-30 22:37:47 +0300

    init commit

---
+++ FloraX/FloraXApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloraXApp.swift
+//  FloraX
+//
+//  Created by İlknur Tulgar on 30.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct FloraXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
